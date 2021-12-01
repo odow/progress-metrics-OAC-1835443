21a130f0413ac686b491a5b88f7d068e0c3fd625
odow <o.dowson@gmail.com>
2021-12-01 14:48:18 +1300

    Updates

--- docs/main.js
+++ docs/main.js
@@ -3,7 +3,7 @@ var d3 = Plotly.d3;
 function load_json(filename, callback) {
     var xml_request = new XMLHttpRequest();
     xml_request.overrideMimeType("application/json");
-    xml_request.open('GET', filename, true);
+    xml_request.open("GET", filename, true);
     // xml_request.setRequestHeader("Access-Control-Allow-Origin","*")
     xml_request.onreadystatechange = function() {
         if (xml_request.readyState == 4) {
@@ -21,6 +21,37 @@ function load_json(filename, callback) {
     xml_request.send(null);
 }
 
+function to_date(d) {
+    function two_digit(x) {
+        if (x < 10) {
+            return "0" + x
+        } else {
+            return x
+        }
+    }
+    return d.getFullYear() + "-" + two_digit(d.getMonth() + 1) + "-" + two_digit(d.getDate());
+}
+
+function add_new_dates(x, y, new_date, new_value) {
+    if (x.length == 0) {
+        x.push(new_date);
+        y.push(new_value);
+    } else if (x[x.length-1] == new_date) {
+        y[y.length-1] = new_value;  // update in-place
+    } else {
+        var date = new Date(x[x.length-1]);
+        new_date = new Date(new_date);
+        while (date < new_date) {
+            date.setDate(date.getDate() + 1);
+            x.push(to_date(date));
+            y.push(y[y.length-1]);
+        }
+        x.push(to_date(new_date));
+        y.push(new_value);
+    }
+    return
+}
+
 function count_of_opened_issues(data, key, is_pr, is_cumulative) {
     i = 0;
     x = [];
@@ -30,15 +61,14 @@ function count_of_opened_issues(data, key, is_pr, is_cumulative) {
             return
         } else if (item["type"] == "opened") {
             i++;
-            x.push(item["date"]);
-            y.push(i);    
+            add_new_dates(x, y, item["date"].slice(0, 10), i);
         } else if (!is_cumulative) {
             i--;
-            x.push(item["date"]);
-            y.push(i);
+            add_new_dates(x, y, item["date"].slice(0, 10), i);
         }
     });
-    object = {name: key, "x": x, "y": y, line: {shape: 'hv'}}
+    add_new_dates(x, y, to_date(new Date()), i);
+    object = {name: key, "x": x, "y": y, stackgroup: "one"}
     if (key != "JuMP.jl" && key != "MathOptInterface.jl") {
         object["visible"] = "legendonly";
     }
@@ -58,10 +88,10 @@ function count_of_users(data, key, is_pr) {
         }
         names.add(item["user"]);
         i++;
-        x.push(item["date"]);
-        y.push(i);
+        add_new_dates(x, y, item["date"].slice(0, 10), i);
     });
-    object = {name: key, "x": x, "y": y, line: {shape: 'hv'}}
+    add_new_dates(x, y, to_date(new Date()), i);
+    object = {name: key, "x": x, "y": y}
     if (key != "JuMP.jl" && key != "MathOptInterface.jl") {
         object["visible"] = "legendonly";
     }
@@ -70,43 +100,46 @@ function count_of_users(data, key, is_pr) {
 
 (function() {
     var charts = [];
+    layout = {
+        margin: {b: 30, t: 20},
+        hovermode: 'closest',
+        "yaxis": {
+            "range": ["2013-01-01", to_date(new Date())],
+            "title": "Count"
+        }
+    }
     load_json("data.json", function (data) {
-        function plot_chart(key, f, layout = {}) {
+        function plot_chart(key, f) {
             var chart = d3.select(key).node();
             var series = Object.keys(data).sort().map(f);
+            console.log(layout);
             Plotly.plot(chart, series, layout);
             charts.push(chart);
             return
         }
         plot_chart(
-            '#chart_count_open_issues', 
+            "#chart_count_open_issues", 
             key => count_of_opened_issues(data, key, false, false),
-            {"yaxis": {"title": "Count"}}
         );
         plot_chart(
-            '#chart_count_open_pull_requests', 
+            "#chart_count_open_pull_requests", 
             key => count_of_opened_issues(data, key, true, false),
-            {"yaxis": {"title": "Count"}}
         );
         plot_chart(
-            '#chart_cumulative_count_open_issues', 
+            "#chart_cumulative_count_open_issues", 
             key => count_of_opened_issues(data, key, false, true),
-            {"yaxis": {"title": "Count"}}
         );
         plot_chart(
-            '#chart_cumulative_count_open_pull_requests', 
+            "#chart_cumulative_count_open_pull_requests", 
             key => count_of_opened_issues(data, key, true, true),
-            {"yaxis": {"title": "Count"}}
         );
         plot_chart(
-            '#chart_count_users_open_issues', 
+            "#chart_count_users_open_issues", 
             key => count_of_users(data, key, false),
-            {"yaxis": {"title": "Count"}}
         );
         plot_chart(
-            '#chart_count_users_open_pull_requests', 
+            "#chart_count_users_open_pull_requests", 
             key => count_of_users(data, key, true),
-            {"yaxis": {"title": "Count"}}
         );
     });
     /* =========================================================================
